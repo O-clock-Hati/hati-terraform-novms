81fc401a23154422fe202d7dbadc89f0b8f09c84
Chou VANG <lesolo91@hotmail.com>
2024-01-31 21:05:19 +0100

    maj main.tf

--- nosvm/main.tf
+++ nosvm/main.tf
@@ -2,7 +2,6 @@ resource "aws_key_pair" "macle" {
   for_each   = var.users
   key_name   = "${each.value.username}-key"
   public_key = each.value.public_key
-  # public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDdkORp8949DkeMkF9aCccSWloQrl6dvdQenNKaReoXhmikB0xzfWWgfdbwzJcARvjE1TFoFiYj/lJ28iH5cPVxE9qlx5fN3w48Zlxv0HrgVWRwXK4TrCvqq63VPPuxwSmIGtzNKqIcf91bhEwb713rtUQZ1el5c7JNZj7wRGvXuOG2RWUJ1u+EX3rpSNtAQ0Rc4mm2FbbvvTQynwWa7Z9pJG2FvYMnSYBEUZAzW+hAVWQu3MTDfz6LS7TEyKYCNLj5fKDT4f6UnIX6gpy0B4fWPst4Lt9aQYQXWfwXybTwnA5qpuYb7mqjap0reDeuizJwx/pJWPH4tIB9goViUZf0RtaPKAA6e/mdF6KWYc5cjS5NJ6W5Z+4ddo7n3Na3abgr9uNEHUi3Bv+jVgRcwywUhwdU1K9DSVjso76tWn5NFyd44WkF1SVH5fhFOktBfIgjJ3ePovsmzqbqQC6ltl/PmCs1nyYGolx/rM+ouP6wiGYr/7k1OhhnrF0G235J5doo1yt4AQkenBXmBBncXRtXrhlrhB1vU8Lhe2GkTIH1W18zDKQyVxMpLNhjm5rfM3UzBLLkolXkk2kKEpjyQW5yXw3qFMJz764T0HXKb39jWDkDtZL6ugYoMDXFpRb44c3MbeAqNXHcVyEERVvQMZwqVmYK4npkpD1yYktxCVLRjw== chouvang@free.fr"
 }
 
 data "aws_ami" "ubuntu" {
@@ -52,7 +51,7 @@ resource "aws_instance" "mavm" {
   ami             = data.aws_ami.ubuntu.id
   instance_type   = "t2.micro"
   key_name        = aws_key_pair.macle[each.key].key_name
-  security_groups = ["${each.value.username}-security-group"] # Utilisation de l'ID du groupe de sécurité correspondant à chaque utilisateur
+  security_groups = ["${each.value.username}-security-group"]
   tags = {
     Name = "${each.value.username}-VM"
   }

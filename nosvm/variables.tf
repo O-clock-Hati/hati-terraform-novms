b0cc419aa771afebd81dc3a9df98046f8c28ef2d
Chou VANG <lesolo91@hotmail.com>
2024-01-31 21:18:31 +0100

    maj users

--- nosvm/variables.tf
+++ nosvm/variables.tf
@@ -4,14 +4,4 @@ variable "users" {
     public_key = string
     hostname   = string
   }))
-  # default = {...} # Ajoute les valeurs par défaut si nécessaire
 }
-
-# variable "users" {
-#   type = list(object({
-#     username   = string
-#     public_key = string
-#     hostname   = string
-#   }))
-#   # default = [...] # Tu peux ajouter une valeur par défaut si nécessaire
-# }
